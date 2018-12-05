a5e87dd8a42f8733c860186241869d1aba542b36
Enki <enki@espada.numericable.fr>
2018-12-05 17:06:45 +0100

    controller new create view new

--- app/controllers/colocs_controller.rb
+++ app/controllers/colocs_controller.rb
@@ -1,7 +1,21 @@
 class ColocsController < ApplicationController
   def new
+    @coloc = Coloc.new
   end
- 
+
   def create
+    @coloc = Coloc.new(coloc_params)
+    @coloc.save
+    if @coloc.save
+      redirect_to plannings_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def coloc_params
+    params.require(:coloc).permit(:name)
   end
 end
--- app/models/coloc.rb
+++ app/models/coloc.rb
@@ -1,4 +1,5 @@
 class Coloc < ApplicationRecord
   has_many :tasks
   has_many :users
+  validates :name, presence: true, uniqueness: true
 end
